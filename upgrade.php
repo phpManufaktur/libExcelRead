9df7619a9c2d1d62ade6e1ae468bbfca1df238fd
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-08-16 09:16:03 +0200

    Added droplet installation

--- upgrade.php
+++ upgrade.php
@@ -31,4 +31,71 @@ else {
 }
 // end include class.secure.php
 
-// nothing to do yet ...
+if (file_exists(WB_PATH . '/modules/droplets/functions.inc.php'))
+  require_once WB_PATH . '/modules/droplets/functions.inc.php';
+
+if (!function_exists('wb_unpack_and_import')) {
+  function wb_unpack_and_import($temp_file, $temp_unzip) {
+    global $admin, $database;
+    // Include the PclZip class file
+    require_once (WB_PATH . '/include/pclzip/pclzip.lib.php');
+    $errors = array();
+    $count = 0;
+    $archive = new PclZip($temp_file);
+    $list = $archive->extract(PCLZIP_OPT_PATH, $temp_unzip);
+    // now, open all *.php files and search for the header;
+    // an exported droplet starts with "//:"
+    if (false !== ($dh = opendir($temp_unzip))) {
+      while (false !== ($file = readdir($dh))) {
+        if ($file != "." && $file != "..") {
+          if (preg_match('/^(.*)\.php$/i', $file, $name_match)) {
+            // Name of the Droplet = Filename
+            $name = $name_match[1];
+            // Slurp file contents
+            $lines = file($temp_unzip . '/' . $file);
+            // First line: Description
+            if (preg_match('#^//\:(.*)$#', $lines[0], $match)) {
+              $description = $match[1];
+            }
+            // Second line: Usage instructions
+            if (preg_match('#^//\:(.*)$#', $lines[1], $match)) {
+              $usage = addslashes($match[1]);
+            }
+            // Remaining: Droplet code
+            $code = implode('', array_slice($lines, 2));
+            // replace 'evil' chars in code
+            $tags = array('<?php', '?>', '<?');
+            $code = addslashes(str_replace($tags, '', $code));
+            // Already in the DB?
+            $stmt = 'INSERT';
+            $id = NULL;
+            $found = $database->get_one("SELECT * FROM " . TABLE_PREFIX .
+                "mod_droplets WHERE name='$name'");
+            if ($found && $found > 0) {
+              $stmt = 'REPLACE';
+              $id = $found;
+            }
+            // execute
+            $result = $database->query("$stmt INTO " . TABLE_PREFIX .
+                "mod_droplets VALUES('$id','$name','$code','$description','" .
+                time() . "','" . $admin->get_user_id() . "',1,0,0,0,'$usage')");
+            if (!$database->is_error()) {
+              $count++;
+              $imports[$name] = 1;
+            }
+            else {
+              $errors[$name] = $database->get_error();
+            }
+          }
+        }
+      }
+      closedir($dh);
+    }
+    return array('count' => $count, 'errors' => $errors, 'imported' => $imports);
+  } // end function wb_unpack_and_import()
+}
+
+global $admin;
+
+if (!file_exists(WB_PATH.'/temp/unzip/')) @mkdir(WB_PATH.'/temp/unzip/');
+wb_unpack_and_import(WB_PATH.'/modules/'.basename(dirname(__FILE__)).'/droplets/droplet_excel.zip', WB_PATH.'/temp/unzip/');
